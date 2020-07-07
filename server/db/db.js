bd7b4ec3cf19cb645e520c2523abc99513490ee4
Andrew VanLoo <c-andrew.vanlloo@charter.com>
2020-07-07 18:13:09 -0400

    db sql update

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/bananagrams', {
-    logging: false
+const db = new Sequelize('bananagrams', 'bngrams', process.env.DBPW, {
+    dialect: 'postgres'
   }
 )
 module.exports = db
