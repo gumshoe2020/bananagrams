13d54f0e978a03556d1ee5287843a486cf414134
Andrew VanLoo <c-andrew.vanlloo@charter.com>
2020-07-07 19:23:27 -0400

    fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-console('dbpw: {}', process.env.DBPW)
+console.info('dbpw: {}', process.env.DBPW)
 const db = new Sequelize('postgres', 'postgres', process.env.DBPW, {
     dialect: 'postgres'
   }
